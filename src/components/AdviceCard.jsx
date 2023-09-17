e705844b9f2be5054f4f32f5774cbc4e0d77c6d0
Abi0l9 <cryptog3ms@gmail.com>
2023-09-17 21:12:46 +0100

    added bookmarking feature

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -8,6 +8,18 @@ const AdviceCard = ({ freshAdvice }) => {
   const [spin, setSpin] = useState(false);
   const [advice, setAdvice] = useState({});
   const [bookmarkedAdvice, setBookmarkedAdvice] = useState([]);
+  const [bkContent, setBkContent] = useState("");
+  // const [showBkmk, setShowBkmk] = useState(false)
+
+  const clearBookMarkContent = () => {
+    setTimeout(() => {
+      setBkContent("");
+    }, 2000);
+  };
+
+  const setBookMarkContent = () => {
+    setBkContent("bookmarked");
+  };
 
   useEffect(() => {
     if (freshAdvice) {
@@ -43,31 +55,40 @@ const AdviceCard = ({ freshAdvice }) => {
         const newAdvs = bookmarkedAdvice.concat(adv);
         setBookmarkedAdvice(newAdvs);
         localStorage.setItem("advices", JSON.stringify(newAdvs));
+        setBookMarkContent();
+        clearBookMarkContent();
       } else {
         const newAdvs = bookmarkedAdvice.concat(adv);
         setBookmarkedAdvice(newAdvs);
         localStorage.setItem("advices", JSON.stringify(newAdvs));
+        setBookMarkContent();
+        clearBookMarkContent();
       }
     }
   };
 
-  console.log(bookmarkedAdvice);
-
   return (
     <div className="relative w-full shadow-md  px-4  flex flex-col items-center md:w-2/4 bg-gray-700 rounded-md ">
-      <div className="absolute top-2 right-2 cursor-pointer ">
-        <Icon
-          icon={bookmarkIcon}
-          onClick={() => bookmarkAdvice(advice)}
-          height={24}
-          width={24}
-          color="white"
-          className=""
-        />
+      <div className="flex flex-row items-center w-full">
+        <div className="text-green-400 text-center text-1xl my-4 w-full">
+          Advice #{advice?.id}
+        </div>
+        <div className="relative flex flex-col items-center cursor-pointer w-[30px]">
+          <Icon
+            icon={bookmarkIcon}
+            onClick={() => bookmarkAdvice(advice)}
+            height={24}
+            width={24}
+            color="white"
+            className=""
+          />
+          <div className="absolute text-[10px] top-5 text-gray-400 px-1">
+            <p className="">{bkContent}</p>
+          </div>
+        </div>
       </div>
-      <div className="text-green-400 text-1xl my-4">Advice #{advice?.id}</div>
       <div className="font-bold text-gray-200 text-center ">
-        "{advice?.advice || "advice loading..."}"
+        {`"${advice?.advice}"` || "advice loading..."}
       </div>
       <div className="w-full flex flex-row items-center my-5">
         <div className="h-[1px] w-full bg-gray-500"></div>
